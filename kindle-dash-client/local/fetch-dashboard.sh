521ad6d4412972a2c0dea9cd56dde89ad22783c0
Cheni Chadowitz <c.chadowitz@gmail.com>
2025-03-10 20:49:59 -0400

    updated kindle files

--- kindle-dash-client/local/fetch-dashboard.sh
+++ kindle-dash-client/local/fetch-dashboard.sh
@@ -3,7 +3,7 @@
 # For example:
 battery_percent=$(gasgauge-info -s)
 # invoke https://YOUR_DOMAIN/battery/27
-"$(dirname "$0")/../xh" -d -q get https://YOUR_DOMAIN/battery/$battery_percent
+"$(dirname "$0")/../xh" -d -q get https://kindle.cheni.dev/battery/$battery_percent
 
 # Fetch the dashboard image
-"$(dirname "$0")/../xh" -d -q -o "$1" get https://YOUR_DOMAIN/dash.png
+"$(dirname "$0")/../xh" -d -q -o "$1" get https://kindle.cheni/dash.png
